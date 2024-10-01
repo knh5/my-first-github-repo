16e03620e2816e76d243ed32c759595b22ddd4e7
Suhrab Shamshiri (s5725515) <s5725515@bournemouth.ac.uk>
2024-10-01 12:28:31 +0100

    initial message

---
+++ my_first_code.py
@@ -0,0 +1,39 @@
+print("hello world")
+print ("goodbye world")
+print ("hello", 12, "goodbye", 12, 1+1 , "1+1")
+
+
+#variables
+#f lets the thing change with variables so it dont print the same thing
+name = "Suhrab"
+Age = 25
+print(f"{name}")
+
+name1 = "alice"
+print("hello " + name1)
+#.format is a function that fills in spaces with given information in this case the two {} with the information given after the .format
+print("{} is {} years old".format("alice",30))
+
+#simple addition with +
+print(265 + 5924)
+#simple subtraction with -
+print(5022-901)
+#simple multiplication with *
+print(5*10)
+#simple division with /
+print(604/4)
+#% finds the remainder when finishing the division
+print(3%2)
+print(10%4)
+#// finds only the integer of the answer so from 2.6 would be 2
+print(9//3)
+print(8//3)
+#** is squared
+print(5**2)
+print(4**3)
+
+print("new thing")
+a = (6*4)
+b = (5**2)
+c = (4%3)
+print(a+b-c)
